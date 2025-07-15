422ee9d63e9823f5f1e4b53f778386bd82c72af8
Abhijit <abhijitgole001@gmail.com>
2025-07-15 23:42:15 +0530

    add doctor api added.

--- ProductManagement/src/main/java/com/example/demo/Demo2Controler.java
+++ ProductManagement/src/main/java/com/example/demo/Demo2Controler.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Demo2Controler {
 
+	
+	String addDoctor() {
+		return "added";
+		
+	}
 }
